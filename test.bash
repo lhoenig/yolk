e71eda635bcb2d5507fc951805af274e17ff57e8
Steven Myint <git@stevenmyint.com>
2013-06-23 10:06:12 -0700

    Make this more portable

--- test.bash
+++ test.bash
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
-yolk --latest-releases=1
-yolk --show-updates
-yolk --query-metadata=yolk
-yolk --depends=pip
+python -m yolk --latest-releases=1
+python -m yolk --show-updates
+python -m yolk --query-metadata=yolk
+python -m yolk --depends=pip
 
 # I would grep to confirm the expected error. But this piece of shit stops
 # printing the error message if I pipe it.
-#yolk --depends=foo 2>&1 | grep 'foo is not installed'
-yolk --depends=foo
+#python -m yolk --depends=foo 2>&1 | grep 'foo is not installed'
+python -m yolk --depends=foo
