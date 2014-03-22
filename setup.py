fca97390581d642b21cb35c4570a9ceca00a2a1a
Steven Myint <git@stevenmyint.com>
2014-03-22 07:27:57 -0700

    Correct information

--- setup.py
+++ setup.py
@@ -2,32 +2,43 @@
 
 """Installer for yolk."""
 
+import ast
+
 from setuptools import setup
 
-from yolk.__init__ import __version__ as VERSION
+
+def version():
+    """Return version string."""
+    with open('yolk/__init__.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
 
 
 with open('README.rst') as readme:
     setup(
         name='yolk3k',
         license='BSD License',
-        version=VERSION,
+        version=version(),
         description='Command-line tool for querying PyPI and Python packages '
                     'installed on your system.',
         long_description=readme.read(),
-        maintainer='Rob Cakebread',
+        maintainer='Steven Myint',
         author='Rob Cakebread',
-        author_email='cakebread @ gmail',
         url='https://github.com/myint/yolk',
         keywords='PyPI,setuptools,cheeseshop,distutils,eggs,package,'
                  'management',
         classifiers=[
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
             'Topic :: Software Development :: Libraries :: Python Modules',
         ],
         packages=['yolk'],
