b26e52162352cf2815bf28219de4f5a7d0bac637
Steven Myint <git@stevenmyint.com>
2014-06-24 06:47:00 -0700

    Preserve metadata order

--- yolk/metadata.py
+++ yolk/metadata.py
@@ -11,6 +11,7 @@ Desc     : Return metadata for Python distribution installed by setuptools
 
 """
 
+import collections
 import email
 
 
@@ -23,19 +24,21 @@ def get_metadata(dist):
     @returns: dict of metadata or None
 
     """
+    metadata = collections.OrderedDict()
+
     if dist.has_metadata('PKG-INFO'):
         text = dist.get_metadata('PKG-INFO')
     elif dist.has_metadata('METADATA'):
         text = dist.get_metadata('METADATA')
     else:
-        return {}
+        return metadata
 
     try:
-        metadata = {}
-        msg = email.message_from_string(text)
-        for header in [l for l in msg._headers]:
+        message = email.message_from_string(text)
+        for header in [l for l in message._headers]:
             metadata[header[0]] = header[1]
     except IOError:
         # No egg-file, installed using other package manager.
         pass
+
     return metadata
