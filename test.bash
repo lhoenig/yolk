0e3c63354581f6df2fea56b345b774987a106dd6
Steven Myint <git@stevenmyint.com>
2014-06-24 06:56:47 -0700

    Confirm output in test

--- test.bash
+++ test.bash
@@ -3,11 +3,11 @@
 python -m yolk --latest-releases=1
 python -m yolk --show-updates
 python -m yolk --show-updates --user
-python -m yolk --query-metadata=yolk
+python -m yolk --query-metadata=autopep8 | grep -i 'license:'
 python -m yolk --depends=pip
 python -m yolk --list
 python -m yolk --list pip
-python -m yolk --list --metadata yolk3k
+python -m yolk --list --metadata autopep8 | grep -i 'license:'
 
 python -m yolk --depends=fake_foo 2>&1 | grep 'fake_foo is not installed'
 
