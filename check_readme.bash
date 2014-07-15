6e5a2ee7bda4b86e250df17ddb7f14de1b999f01
Steven Myint <git@stevenmyint.com>
2014-07-14 19:06:00 -0700

    Clean up beforehand

--- check_readme.bash
+++ check_readme.bash
@@ -1,4 +1,13 @@
 #!/bin/bash -eux
 
+clean()
+{
+    rm -rf ./Paste-*.tar.gz ./Paste_svn
+}
+
+clean
+
 grep '\$' README.rst | grep -iv install | grep -iv upgrade | \
     sed 's/\$/python -m /' | bash -eux
+
+clean
