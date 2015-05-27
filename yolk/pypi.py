5e763aa39237041d0d9169b786a8270935fc4bbb
Steven Myint <git@stevenmyint.com>
2015-05-27 07:15:43 -0700

    Remove unused code

--- yolk/pypi.py
+++ yolk/pypi.py
@@ -29,8 +29,6 @@ from yolk.utils import get_yolk_dir
 
 XML_RPC_SERVER = 'https://pypi.python.org/pypi'
 
-MAX_CACHE_AGE = 60
-
 
 class ProxyTransport(xmlrpclib.Transport):
 
@@ -158,13 +156,6 @@ class CheeseShop(object):
 
     def fetch_pkg_list(self, ignore_cache=False):
         """Fetch and cache master list of package names from PYPI."""
-        if (
-            not ignore_cache and
-            os.path.exists(self.pkg_cache_file) and
-            time.time() - os.stat(self.pkg_cache_file).st_mtime < MAX_CACHE_AGE
-        ):
-            return
-
         package_list = self.list_packages()
         with open(self.pkg_cache_file, 'w') as output_file:
             print(package_list, file=output_file)
